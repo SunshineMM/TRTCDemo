ac928a5e6f9de83b274eab0d98050e093f863bb0
刘健 <liujian@98du.com>
2022-06-14 18:01:53 +0800

    添加SDK,添加权限判断

--- app/src/main/java/com/example/trtcdemo/MainActivity.kt
+++ app/src/main/java/com/example/trtcdemo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.trtcdemo
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.guolindev.PermissionX
+import com.tencent.rtmp.ui.TXCloudVideoView
+import com.tencent.trtc.TRTCCloud
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PermissionX.init(this)
+            .permissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE)
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "授予所有权限", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "这些权限被拒绝: $deniedList", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
